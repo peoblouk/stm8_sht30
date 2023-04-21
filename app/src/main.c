59c752e3319f8862bd385a0315ccb5fdca4cd616
peoblouk <ahojcz48@gmail.com>
2023-04-21 14:43:49 +0200

    working with lm75a

--- app/src/main.c
+++ app/src/main.c
@@ -1,59 +1,66 @@
-#include <stdio.h>
 #include "stm8s.h"
 #include "delay.h"
 #include "LCD_I2C.h"
-#include "sht30.h"
+#include "milis.h"
+#include "lm75a.h"
 
-//! Makra
 // Indikační LED
-#define SHT30_ADDRESS_1 0x44 // BME280 I2C Address
-// #define SHT30_ADDRESS_2 0x45 // BME280 I2C Address
+#define LED_PORT GPIOD
+#define LED_PIN GPIO_PIN_4
 
-//! Proměnné
-// uint16_t mtime_key = 0;                    // Proměnná pro millis
-// float humidity, temperature; // Proměnné pro teplotu a vlhkost
+#define TEPLOMER 0x49
 
-//! Uživatelské funkce
 void setup(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
-    delay_init(); // Incializace časovače TIM4
-    // init_milis(); // Iniciaizace millis TIM2
-    // Serial_Begin(9600);
-    SHT30_Init(SHT30_ADDRESS_1);        // Inicilaizace sht30
-    LCD_I2C_Init(0x27, 16, 2);          // Inicializace LCD
-    LCD_I2C_Print("Ultra teploměr..."); // Úvodní obrazovka na displej
-    GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW);
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);               // Předdělička DIV1
+    delay_init();                                                // Incializace časovače TIM4
+    init_milis();                                                // Iniciaizace millis TIM2
+    GPIO_Init(LED_PORT, LED_PIN_RED, GPIO_MODE_OUT_PP_LOW_SLOW); // Pin LED RED
+    LCD_I2C_Init(0x27, 16, 2);                                   // Inicializace LCD
+    LM75A_Init(TEPLOMER);                                        // Inicializace teploměrů
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);     // Inicializace LED
+    LCD_I2C_Print("Ultra Patrik");
     delay_ms(1000);
-    LCD_I2C_Clear();
+    LCD_I2C_Clear
 }
 
-//! Main program loop
+void blink(void)
+{
+    GPIO_WriteReverse(LED_PORT, LED_PIN);
+    delay_ms(20);
+    GPIO_WriteReverse(LED_PORT, LED_PIN);
+}
+
 int main(void)
 {
-    setup();                 // Inicializace všech periferií
-    LCD_I2C_SetCursor(0, 0); // Nastavení kurzor
-    uint8_t temperature_1[2];
-    uint8_t humidity_1[2];
+    uint8_t data_teplomer[2]; // Proměnná pro uložení teploty
+    uint16_t cas_Ted = 0;     // Proměnná pro millis
+    char buffer[48];          // Proměnná pro zápis na displej
+    uint8_t posun = 0;
 
+    setup();                    // Inicializace všech periferií
+    LCD_I2C_SetCursor(0, 0);    // Nastavení kurzoru
+    LCD_I2C_Print("Teplota :"); // Úvodní obrazovka na displej
     while (1)
     {
-        char buffer1[48];
-        char buffer2[48];
+        if ((get_milis() - cas_Ted) > 2000)
+        {
+            cas_Ted = get_milis();                          // Milis now
+            LM75A_ReadTemperature(TEPLOMER, data_teplomer); // Čtení teploty
 
-        SHT30_ReadRegister(SHT30_ADDRESS_1, temperature_1[0], temperature_1[1], humidity_1[0], humidity_1[1]);
-        // SHT30_ReadRegister(SHT30_ADDRESS_2, temperature_1[0], temperature_1[1], humidity_1[0], humidity_1[1]);
+            if (posun <= 4)
+            {
+                posun = 0;
+            }
+            else
+            {
+                posun++;
+            }
 
-        GPIO_WriteReverse(GPIOD, GPIO_PIN_4);
-        LCD_I2C_SetCursor(0, 0); // Nastavení kurzoru
-        sprintf(buffer1, "temp= %d.%d C", temperature_1[0], temperature_1[1]);
-        LCD_I2C_Print(buffer1);
-        // Serial_Print_String(buffer1);
-
-        LCD_I2C_SetCursor(0, 1); // Nastavení kurzoru
-        sprintf(buffer2, "Hum= %d.%d %%", humidity_1[0], humidity_1[1]);
-        LCD_I2C_Print(buffer2);
-
-        delay_ms(1000);
+            LCD_I2C_Clear();
+            LCD_I2C_SetCursor(posun, 1);                                        // Nastavení kurzoru
+            sprintf(buffer, "T = %d.%d C", data_teplomer[0], data_teplomer[1]); // Zformátování stringu
+            LCD_I2C_Print(buffer);                                              // Vytiskni na displej
+        }
     }
 }
